4aed58032103c174893b206492c01a3984c66a4f
Cheng xiaoxiao <iquanzhan@163.com>
2020-01-21 22:25:50 +0800

    init web

---
+++ admin-web/src/main/java/com/chengxiaoxiao/web/MainApplication.java
@@ -0,0 +1,18 @@
+package com.chengxiaoxiao.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @Author: Cheng XiaoXiao  (🍊 ^_^ ^_^)
+ * @Date: 2020/1/21 9:40 下午
+ * @Description:
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
