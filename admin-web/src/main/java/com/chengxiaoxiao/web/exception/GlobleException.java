fe48fa522bb06521e8bb275f747fe28a9116fbd6
Cheng xiaoxiao <iquanzhan@163.com>
2020-01-31 22:30:56 +0800

    [Web]添加lombok

--- admin-web/src/main/java/com/chengxiaoxiao/web/exception/GlobleException.java
+++ admin-web/src/main/java/com/chengxiaoxiao/web/exception/GlobleException.java
@@ -1,17 +1,14 @@
 package com.chengxiaoxiao.web.exception;
 
 import com.chengxiaoxiao.model.common.dtos.CodeMsg;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class GlobleException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
-    @Getter
     private CodeMsg cm;
-
-    public GlobleException(CodeMsg codeMsg) {
-        super(codeMsg.toString());
-        this.cm = codeMsg;
-    }
 }
