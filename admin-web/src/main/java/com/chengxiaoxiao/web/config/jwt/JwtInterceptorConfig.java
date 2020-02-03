b1617612e338a6939237ec2ae9c06d08472c18ad
Cheng xiaoxiao <iquanzhan@163.com>
2020-02-03 20:51:32 +0800

    [用户模块]修复swagger不显示问题

--- admin-web/src/main/java/com/chengxiaoxiao/web/config/jwt/JwtInterceptorConfig.java
+++ admin-web/src/main/java/com/chengxiaoxiao/web/config/jwt/JwtInterceptorConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
@@ -26,6 +27,14 @@ public class JwtInterceptorConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtIntercptor).addPathPatterns("/**").excludePathPatterns("/login");
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
 
