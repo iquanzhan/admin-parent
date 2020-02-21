a026622f99628970f9381a3a7737d78e26db25b7
Cheng xiaoxiao <iquanzhan@163.com>
2020-02-21 22:16:04 +0800

    [跨域访问]：添加全局跨域访问

---
+++ admin-web/src/main/java/com/chengxiaoxiao/web/config/GlobalCorsConfig.java
@@ -0,0 +1,35 @@
+package com.chengxiaoxiao.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 全局开启跨域访问
+ *
+ * @Author: Cheng XiaoXiao  (🍊 ^_^ ^_^)
+ * @Date: 2020/2/21 10:13 下午
+ * @Description:
+ */
+@Configuration
+public class GlobalCorsConfig {
+
+    private CorsConfiguration addCorsConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //请求常用的三种配置，*代表允许所有，当时你也可以自定义属性（比如header只能带什么，只能是post方式等等）
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", addCorsConfig());
+        return new CorsFilter(source);
+    }
+}
+
